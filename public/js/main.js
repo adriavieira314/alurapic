d39c22264326fa230f4b566b56a0b953ff12b6ee
Adria Vieira <adriavieira731@gmail.com>
2020-11-30 14:28:54 -0400

    Rotas do Angular

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,18 @@
 //entre colchetes devo chamar os modules que minha aplicação vai precisar
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+    .config(function($routeProvider, $locationProvider) {
+
+        $locationProvider.html5Mode(true);
+        //minha rota principal
+        $routeProvider.when('/fotos', {
+            templateUrl: 'partials/principal.html',
+            controller: 'FotosController'
+        });
+
+        $routeProvider.when('/foto/new', {
+            templateUrl: 'partials/foto.html'
+        });
+
+        //caso o usuario digite um endereco que não exista, ele será redirecionado para rota principal
+        $routeProvider.otherwise({ redirectTo: '/fotos' });
+    });
