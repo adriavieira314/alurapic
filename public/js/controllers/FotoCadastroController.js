998d21ceb07021e3bb1d7a0948266814e8b19baa
Adria Vieira <adriavieira731@gmail.com>
2020-12-01 10:39:18 -0400

    validaçao e cadastro de novas imagens

--- public/js/controllers/FotoCadastroController.js
+++ public/js/controllers/FotoCadastroController.js
@@ -1,10 +1,21 @@
-angular.module('alurapic').controller('FotoCadastroController', ['$scope', function ($scope) {
+angular.module('alurapic').controller('FotoCadastroController', ['$scope', '$http', function ($scope, $http) {
 
     //quando vazio e usado um ng-model  na view, o objeto é criado automaticamente
     $scope.foto = {};
+    $scope.mensagem = '';
 
     $scope.submeter = function() {
-        console.log($scope.foto);
+        if ($scope.formulario.$valid) {
+            $http.post('/v1/fotos', $scope.foto)
+            .success(function() {
+                $scope.foto = {};
+                $scope.mensagem = 'Foto adicionada com sucesso!';
+            })
+            .error(function(erro) {
+                console.log(erro);
+                $scope.mensagem = 'Não foi possível cadastrar a imagem!';
+            })
+        }
     };
 
 }]);
